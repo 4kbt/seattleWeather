0c90ba856af8a80138a0b270ff5c1c1c329443f0
cndnb <blinnc@uw.edu>
2017-07-10 11:39:03 -0700

    Add files via upload

--- AveragedDayBins.m
+++ AveragedDayBins.m
@@ -8,7 +8,7 @@ N=[];
 n=1;
 for i=1:12
   while (d(n,2) == i)
-    out = [out; d(n,2),d(n,3),0,0,0];
+    out = [out; 0,0,0,0];
     N = [N; 0,0,0];
     n=n+1;
   endwhile
@@ -17,11 +17,15 @@ endfor
 %Sorts data into bins
 counter=1;
 for indexYear=d(1,1):d(length(d),1)
+  %From 1-366 (for leap day)
   daysCounter=1;
   for indexMonth=1:12
     while (d(counter,2)==indexMonth&counter<25371)
-      out(daysCounter,3:5)=[(out(daysCounter,3)+d(counter,4)), (out(daysCounter,4)
-                            +d(counter,5)), (out(daysCounter,5)+d(counter,6))];
+      %Adds each day to its corresponding bin, the first matrix element 
+      %needs to be optimized
+      out(daysCounter,:)=[(daysCounter*24*60*60),(out(daysCounter,2)+d(counter,4)),(out(daysCounter,3)+d(counter,5)), (out(daysCounter,4)+d(counter,6))];
+      
+      %Counts the number of elements in each bin
       N(daysCounter,:)=N(daysCounter,:)+ones(1,3);
       counter = counter+1;
       daysCounter=daysCounter+1;
@@ -31,9 +35,21 @@ endfor
 
 %This performs the average of each day
 for a=1:length(out)
-  out(a,3)=out(a,3)/N(a,1);
-  out(a,4)=out(a,4)/N(a,2);
-  out(a,5)=out(a,5)/N(a,3);
+  out(a,2)=out(a,2)./N(a,1);
+  out(a,3)=out(a,3)./N(a,2);
+  out(a,4)=out(a,4)./N(a,3);
 endfor
 
-plot(out(:,2),out(:,3));
+%This plots the average pressure from 1948-2017 for each day of the year
+%plot(out(:,1),out(:,2));
+
+%Average temp (high and low) on the time domain
+
+dayFreqPressure = psd(out(:,1),out(:,2));
+dayFreqTempH = psd(out(:,1),out(:,3));
+dayFreqTempL = psd(out(:,1),out(:,4));
+
+%loglog(dayFreqPressure(:,1),dayFreqPressure(:,2));
+loglog(dayFreqTempH(:,1),dayFreqTempH(:,2),
+       dayFreqTempL(:,1),dayFreqTempL(:,2));
+
--- daysPlot.m
+++ daysPlot.m
@@ -1,1 +1,10 @@
-plot(d(:,1),d(:,4));
+new = d(:,3:6);
+for counter = 1:length(d(:,1))
+  new(counter,1) = counter*24*60*60;
+endfor
+%newFreqP = psd(new(:,1),new(:,2));
+%newTH = psd(new(:,1),new(:,3)-mean(new(:,3)));
+%newTL = psd(new(:,1),new(:,4)-mean(new(:,4)));
+
+%loglog(newTH(:,1),newTH(:,2),
+%       newTL(:,1),newTL(:,2));
