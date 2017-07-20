42a914d86e17c7463c7177f812f57b749290cb47
cndnb <blinnc@uw.edu>
2017-07-20 15:13:00 -0700

    Implemented plotting and errors
    Also added a variable for difference between months for easy changing of comparison months.

--- fixedMonthAverageCorrelation.m
+++ fixedMonthAverageCorrelation.m
@@ -42,65 +42,107 @@ for m = 1:12
   meanSubtractedMonths(mDex,7)  = individualMonths(mDex,7)- monthAverages(m,3);
 end
 %
-%Plots all figures for high temp, low temp, and precipitation
+%Plots all figures for high temp, low temp, and precipitation:
+%This is the difference between months 1<=diff<=11
+diff = 1;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Precipitation
 figure(1)
 hold on
 PrecipData = [];
-for m=1:11
+for m=1:(12-diff)
   PrecipData = [PrecipData; meanSubtractedMonths(meanSubtractedMonths(:,2)== m,3:4)...
-                meanSubtractedMonths(meanSubtractedMonths(:,2)==(m+1),3:4)];
+                meanSubtractedMonths(meanSubtractedMonths(:,2)==(m+diff),3:4)];
   plot(meanSubtractedMonths(meanSubtractedMonths(:,2)== m   ,3), 
-       meanSubtractedMonths(meanSubtractedMonths(:,2)==(m+1),3),'+');
+       meanSubtractedMonths(meanSubtractedMonths(:,2)==(m+diff),3),'+');
             
 end
+%Covariance
+covarP = sum(PrecipData(:,1).*PrecipData(:,3))/length(PrecipData(:,1));
+
+%pearson correlation: covariance/mean(sigma_x*sigma_y)
+pearsonPrecip = mean(covarP/(PrecipData(:,2).*PrecipData(:,4)))
+
 %calculation of line of best fit
-finalPrecip = ols2(PrecipData(:,3),PrecipData(:,1));
-finalPrecip
-pearsonPrecip = mean(finalPrecip(:,5)/(meanSubtractedMonths(:,2)*meanSubtractedMonths(:,4)));
+[betaP sigmaP arrP errP covP] = ols2(PrecipData(:,3),PrecipData(:,1));
+timeP = -.01:.001:.01;
+errorbar(timeP,betaP*timeP,errP,'k');
+
+%Start plotting figure 1
 hold off
 
-legend('Pearson Correlation =', pearsonPrecip);
-title('Precipitation Comparison Between Months');
+%Labels
+legend('Colored dots: monthly anomaly','black line: ols fit');
+title(strcat('Precipitation Comparison Between Months, Month diff =',num2str(diff)));
 xlabel('Current Month Precipitation Anomaly');
 ylabel('Next Month Precipitation Anomaly');
 axis("square");
 grid on
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %High Temperatures
 figure(2)
 hold on
-for m=1:11
-
+HighTData = [];
+for m=1:(12-diff)
+  HighTData = [HighTData; meanSubtractedMonths(meanSubtractedMonths(:,2)== m,5:6)...
+                meanSubtractedMonths(meanSubtractedMonths(:,2)==(m+diff),5:6)];
   plot(meanSubtractedMonths(meanSubtractedMonths(:,2)== m   ,5),
-       meanSubtractedMonths(meanSubtractedMonths(:,2)==(m+1),5), '+');
+       meanSubtractedMonths(meanSubtractedMonths(:,2)==(m+diff),5), '+');
             
 end
-%calculation of line of best fit
+%Covariance
+covarH = sum(HighTData(:,1).*HighTData(:,3))/length(HighTData(:,1));
 
+%pearson correlation: covariance/mean(sigma_x*sigma_y)
+pearsonHigh = (covarH*length(HighTData(:,1)))/sum(HighTData(:,2).*HighTData(:,4))
+
+%calculation of line of best fit
+[betaH sigmaH arrH errH covH] = ols2(HighTData(:,3),HighTData(:,1));
+timeH = -10:1:10;
+errorbar(timeH,betaH*timeH,errH,'k');
+
+%Start plotting figure 2
 hold off
 
-title('High Temperature Comparison Between Months');
+%Labels
+legend('Colored dots: monthly anomaly','black line: ols fit');
+title(strcat('High Temperature Comparison Between Months, Month diff =',num2str(diff)));
 xlabel('Current Month Temp Anomaly');
 ylabel('Next Month Temp Anomaly');
 axis("square");
 grid on
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Low Temperatures
 figure(3)
 hold on
-for m=1:11
-
+LowTData = [];
+for m=1:(12-diff)
+  LowTData = [LowTData; meanSubtractedMonths(meanSubtractedMonths(:,2)== m,7:8)...
+                meanSubtractedMonths(meanSubtractedMonths(:,2)==(m+diff),7:8)];
   plot(meanSubtractedMonths(meanSubtractedMonths(:,2)==m,7),
-       meanSubtractedMonths(meanSubtractedMonths(:,2)==(m+1),7),'+');
+       meanSubtractedMonths(meanSubtractedMonths(:,2)==(m+diff),7),'+');
             
 end
-%calculation of line of best fit
+%Covariance
+covarL = sum(LowTData(:,1).*LowTData(:,3))/length(LowTData(:,1));
 
+%pearson correlation: covariance/mean(sigma_x*sigma_y)
+pearsonLow = (covarL*length(LowTData(:,1)))/sum(LowTData(:,2).*LowTData(:,4))
+
+%calculation of line of best fit
+[betaL sigmaL arrL errL covL] = ols2(LowTData(:,3),LowTData(:,1));
+timeL = -10:1:10;
+errorbar(timeL,betaL*timeL,errL,'k');
+
+%Start plotting figure 3
 hold off
 
-title('Low Temperature Comparison Between Months');
+%Labels
+legend('Colored dots: monthly anomaly','black line: ols fit');
+title(strcat('Low Temperature Comparison Between MonthsMonth diff =',num2str(diff)));
 xlabel('Current Month Temp Anomaly');
 ylabel('Next Month Temp Anomaly');
 axis("square");
