5830c8668d20a97363b7df8eca89ca542547be02
charlie <charlie@dicke.nanoradian.com>
2017-09-08 12:47:26 -0700

    Adds all of the average computation. Looks okay.

--- analyzeSeatacRecords.m
+++ analyzeSeatacRecords.m
@@ -15,12 +15,15 @@ end
 climateAverages = climateAverages(:,[2 4:6]);
 
 %let's see if the averages make sense
-plot(d(:,2), d(:,6),'.', climateAverages(:,1), climateAverages(:,4),
-     d(:,2)+.1, d(:,5),'.', climateAverages(:,1)+.1,climateAverages(:,3) );
+plot(d(:,2), d(:,6),'.', climateAverages(:,1), climateAverages(:,4), ';ave low;',
+     d(:,2)+.1, d(:,5),'.', climateAverages(:,1)+.1,climateAverages(:,3) ,
+	';ave high;');
+xlabel('month');
+ylabel('temperature')
 %looks good
 
 %Now, we need month-by-month averages
-monthlyAverage = []
+monthlyAverage = [];
 for year = min(d(:,1)):max(d(:,1))
 	for month = 1:12
 		monthlyAverage = [ monthlyAverage; year month ...
@@ -28,3 +31,12 @@ for year = min(d(:,1)):max(d(:,1))
 	end %month
 end %year
 
+%let's see if the monthly averages look good.
+plot( d(:,1) + d(:,2) / 12 + d(:,3) / 30 / 12 , d(:,5),'.', ... 
+      monthlyAverage(:,1) + monthlyAverage(:,2) / 12 + 0.5/12, monthlyAverage(:,4))
+%looks okay
+
+%first chance we have to really look at precip:
+plot( monthlyAverage(:,2), monthlyAverage(:,3),'.',
+      climateAverages(:,1), climateAverages(:,2))
+%looks okay
